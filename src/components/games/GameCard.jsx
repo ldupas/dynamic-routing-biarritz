4fe53e5516c846eecb6a0cb9091a24a908a87f32
Your Name <louisdupas@net-c.com>
2023-04-20 15:37:02 +0200

    filter added

--- src/components/games/GameCard.jsx
+++ src/components/games/GameCard.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import './GameCard.css';
 
 const GameCard = ({ game }) => {
-    const { id, name, background_image } = game;
+    const { id, name, rating, background_image } = game;
 
     return (
         <div>
             <Link to={`/${id}`}>
                 <h2>{name}</h2>
-                <img src={background_image} alt={name} />
             </Link>
+                <p>Rating : {rating}</p>
+                <img src={background_image} alt={name} className="game-img"/>
         </div>
     )
 }
--- src/components/games/GameList.jsx
+++ src/components/games/GameList.jsx
@@ -1,22 +1,45 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import GameCard from './GameCard';
+import './GameList.css';
 
 const GameList = () => {
     const [games, setGames] = useState([]);
+    const [isFiltered, setIsFiltered] = useState(false);
 
     useEffect(() => {
         axios.get('https://apis.wilders.dev/wild-games/games/')
         .then((res) => setGames(res.data))
-    }, [])
+    }, []);
+
+    const toggleFilter = () => {
+        setIsFiltered(!isFiltered)
+    };
 
     return (
-        <div>
+        <div className='GameList'>
+            <button onClick={toggleFilter}>{isFiltered ? 'All games' : 'Best Games'}</button>
             {
-                games &&
-                games.map((game) => (
+                games && isFiltered ?
+                <div className='game-card'>
+                {
+                games
+                .filter((game) => (game.rating >= 4.25))
+                .map((game) => (
+                    <GameCard key={game.id} game={game} id={game.id} />
+                )) 
+                }
+                </div>
+                :
+
+                <div className='game-card'>
+                {
+                games
+                .map((game) => (
                     <GameCard key={game.id} game={game} id={game.id} />
                 ))
+                }
+                </div>
             }
         </div>
     )
